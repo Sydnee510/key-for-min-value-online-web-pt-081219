56da1eb9912d9cd26de38f75c0e1e6425d80336c
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-24 00:29:27 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,14 +12,4 @@
  end
  low_key
  end
-# def key_for_min_value(hash)
-#   lowest_key = nil
-#   lowest_value = Float::INFINITY
-#   hash.each do |k, v|
-#     if v < lowest_value
-#       lowest_value = v
-#       lowest_key = k
-#     end
-#   end
-#   lowest_key
-# end
+
